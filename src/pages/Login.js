e8e58a852cf19086f6530876920bf3feb1e8a7aa
rileyjdenton <rileyd315@gmail.com>
2024-11-04 15:51:10 -0500

    testing

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:5000/api/login', {
+      const response = await axios.post('https://your-app-name.herokuapp.com/api/login', {
         email,
         password,
       });
