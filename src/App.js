11bcc13be842e032c7eafe1e08dc7031daee1870
rileyjdenton <rileyd315@gmail.com>
2024-11-05 09:03:24 -0500

    Altered index.html

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
@@ -6,15 +7,24 @@ import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/Register" element={<Register />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </Router>
+    <div style={{
+      backgroundColor: '#0b3864', // Change to your desired color
+      minHeight: '100vh', // Ensures the background fills the viewport
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
 export default App;
+
 
