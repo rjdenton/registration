ccbd82bdd1a5ce89ce27edac1d6dfe1727cc08ba
rileyjdenton <rileyd315@gmail.com>
2024-11-05 09:17:07 -0500

    Altered index.html

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import './App.css'; // Import your CSS file here
 
 function App() {
   return (
-    <div style={{ width: '100%', height: '100%' }}> {/* Full width and height wrapper */}
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
