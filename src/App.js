2a436dc136a04f71a066ce5b4285d0c34c5991ce
rileyjdenton <rileyd315@gmail.com>
2024-11-05 09:13:21 -0500

    Altered index.html

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import './App.css'; // Import your CSS file here
 
 function App() {
   return (
-    <div style={{
-      backgroundColor: '#0b3864', // Change to your desired color
-      minHeight: '100vh', // Ensures the background fills the viewport
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
+    <div style={{ width: '100%', height: '100%' }}> {/* Full width and height wrapper */}
       <Router>
         <Routes>
           <Route path="/" element={<Homepage />} />
@@ -26,5 +21,6 @@ function App() {
 }
 
 export default App;
+
 
 
